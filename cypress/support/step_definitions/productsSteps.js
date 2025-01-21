515b636ee5305a8746aeeb4e9cae8a3e4b1afe4b
Marcel Aleixo <aleixo1105gmail.com>
2025-01-21 17:24:43 -0400

    add Products Tests

---
+++ cypress/support/step_definitions/productsSteps.js
@@ -0,0 +1,37 @@
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
+import ProductsPage from '../../pages/productsPage'; // Importando a instância de productsPage
+
+import { performLogin } from './loginSteps';
+
+Given('I am logged in with valid credentials', () => {
+  performLogin(); // Realiza o login
+});
+
+When('Should ensure that the products layout is structured in a grid', function() {
+  ProductsPage.validateGridLayout();
+});
+
+// Step Definition para verificar visibilidade e formato do nome
+When('Should ensure that all product names are visible', function() {
+  ProductsPage.validateProductNameVisibility();
+});
+
+// Step Definition para verificar visibilidade da descrição
+When('Should ensure that all product descriptions are visible', function() {
+  ProductsPage.validateProductDescriptionVisibility();
+});
+
+// Step Definition para verificar visibilidade e formato do preço
+When('Should ensure that all prices are visible', function() {
+  ProductsPage.validatePriceVisibility();
+});
+
+When('Should ensure that all product images are visible', function() {
+  ProductsPage.validateProductImageVisibility();
+});
+
+// Step Definition para verificar visibilidade do botão de adicionar ao carrinho
+When('Should ensure that all add to cart buttons are visible', function() {
+  ProductsPage.validateAddToCartButtonVisibility();
+});
+
